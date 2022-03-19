b58b7c7716b6bf8d5412f42273e00109e834ba6a
LeoXing <leoxing1996@gmail.com>
2022-03-19 23:01:57 +0800

    add baseline config for s2

---
+++ train_s2_compCar_baseline.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -x
+
+
+PARTITION=mm_lol
+JOB_NAME=s2-compCar-baseline
+
+CONFIG=stylegan2
+WORK_DIR=./out
+
+
+GPUS=${GPUS:-8}
+GPUS_PER_NODE=${GPUS_PER_NODE:-8}
+CPUS_PER_TASK=${CPUS_PER_TASK:-5}
+PY_ARGS=${@:5}
+SRUN_ARGS=${SRUN_ARGS:-""}
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+srun -p ${PARTITION} \
+    --job-name=${JOB_NAME} \
+    --gres=gpu:${GPUS_PER_NODE} \
+    --ntasks=${GPUS} \
+    --ntasks-per-node=${GPUS_PER_NODE} \
+    --cpus-per-task=${CPUS_PER_TASK} \
+    --kill-on-bad-exit=1 \
+    ${SRUN_ARGS} \
+    python train.py --outdir=${WORK_DIR} --cfg=${CONFIG} --gpus=${GPUS} \
+                    --slurm --aug noaug --gamma 10 --kimg 57000 \
+                    --data s3://data/compCar256/
