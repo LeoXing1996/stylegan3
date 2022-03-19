1cb4f8f825b66b84580c6c30bd941a063980d540
LeoXing <leoxing1996@gmail.com>
2022-03-19 22:51:48 +0800

    add cache path for cluster

--- dnnlib/util.py
+++ dnnlib/util.py
@@ -126,6 +126,9 @@ def make_cache_dir_path(*paths: str) -> str:
         return os.path.join(_dnnlib_cache_dir, *paths)
     if 'DNNLIB_CACHE_DIR' in os.environ:
         return os.path.join(os.environ['DNNLIB_CACHE_DIR'], *paths)
+    # NOTE: we have add `cache` to our PATH in `.zshrc`
+    if 'cache' in os.environ:
+        return os.path.join(os.environ['cache'], '.cache', 'dnnlib', *paths)
     if 'HOME' in os.environ:
         return os.path.join(os.environ['HOME'], '.cache', 'dnnlib', *paths)
     if 'USERPROFILE' in os.environ:
