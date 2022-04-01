ecec4591161c36315a278b355d091a52cf9d5be4
LeoXing <leoxing1996@gmail.com>
2022-04-01 14:31:17 +0800

    add config of nerf-gan

--- configs/s2_incident1k_nerf_from_scratch_ada.sh
+++ configs/s2_incident1k_nerf_from_scratch_ada.sh
@@ -8,4 +8,4 @@ CONFIG=stylegan2
 BATCH=32
 KIMG=57000
 
-PYTHON_ARGS="--gamma 10 --nerf_config=nerf_configs/car_pretrain_nerf_s2.yml"
+PYTHON_ARGS="--gamma 10 --nerf_config=nerf_configs/car_from_scratch_nerf_s2.yml"
